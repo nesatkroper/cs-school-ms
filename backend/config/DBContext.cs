1c719a40515a8a420caf737750d8ed3cf2a21f65
megalobob <phanunsuon@gmail.com>
2024-09-17 20:21:54 +0700

    first commit

---
+++ backend/config/DBContext.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace backend.config
+{
+    public class DBContext
+    {
+        static SqlConnection conn;
+
+        public static void ConnOpen()
+        {
+            if (conn == null)
+            {
+                conn = new SqlConnection();
+                conn.ConnectionString = @"Data Source=DESKTOP-BRGH90J\SQLEXPRESS;Initial Catalog=POS;Integrated Security=True;";
+                conn.Open();
+            }
+        }
+
+        public static void ConnClose()
+        {
+            if (conn != null)
+                conn.Close();
+        }
+
+        public static SqlConnection ConnGet()
+        {
+            if (conn == null)
+                ConnOpen();
+            return conn;
+        }
+    }
+}
---
+++ cs-school-ms/Program.cs
@@ -0,0 +1,23 @@
+﻿using cs_school_ms.view;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace cs_school_ms
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Homepage());
+        }
+    }
+}
